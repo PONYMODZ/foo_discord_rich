c4fb1db1e16da916fcd489f628cb69244975a76f
unknown <limnaios1@gmail.com>
2023-02-06 20:20:34 +0100

    try 4

--- foo_discord_rich/ui/ui_pref_tab_main.cpp
+++ foo_discord_rich/ui/ui_pref_tab_main.cpp
@@ -19,6 +19,7 @@ PreferenceTabMain::PreferenceTabMain( PreferenceTabManager* pParent )
     , largeImageSettings_( config::largeImageSettings,
                            { { ImageSetting::Light, IDC_RADIO_IMG_LIGHT },
                              { ImageSetting::Dark, IDC_RADIO_IMG_DARK },
+                             { ImageSetting::Url, IDC_RADIO_IMG_URL },
                              { ImageSetting::Disabled, IDC_RADIO_IMG_DISABLED } } )
     , smallImageSettings_( config::smallImageSettings,
                            { { ImageSetting::Light, IDC_RADIO_PLAYBACK_IMG_LIGHT },
@@ -34,7 +35,7 @@ PreferenceTabMain::PreferenceTabMain( PreferenceTabManager* pParent )
           qwr::ui::CreateUiDdxOption<qwr::ui::UiDdx_CheckBox>( isEnabled_, IDC_CHECK_IS_ENABLED ),
           qwr::ui::CreateUiDdxOption<qwr::ui::UiDdx_TextEdit>( stateQuery_, IDC_TEXTBOX_STATE ),
           qwr::ui::CreateUiDdxOption<qwr::ui::UiDdx_TextEdit>( detailsQuery_, IDC_TEXTBOX_DETAILS ),
-          qwr::ui::CreateUiDdxOption<qwr::ui::UiDdx_RadioRange>( largeImageSettings_, std::initializer_list<int>{ IDC_RADIO_IMG_LIGHT, IDC_RADIO_IMG_DARK, IDC_RADIO_IMG_DISABLED } ),
+          qwr::ui::CreateUiDdxOption<qwr::ui::UiDdx_RadioRange>( largeImageSettings_, std::initializer_list<int>{ IDC_RADIO_IMG_LIGHT, IDC_RADIO_IMG_DARK, IDC_RADIO_IMG_URL, IDC_RADIO_IMG_DISABLED } ),
           qwr::ui::CreateUiDdxOption<qwr::ui::UiDdx_RadioRange>( smallImageSettings_, std::initializer_list<int>{ IDC_RADIO_PLAYBACK_IMG_LIGHT, IDC_RADIO_PLAYBACK_IMG_DARK, IDC_RADIO_PLAYBACK_IMG_DISABLED } ),
           qwr::ui::CreateUiDdxOption<qwr::ui::UiDdx_RadioRange>( timeSettings_, std::initializer_list<int>{ IDC_RADIO_TIME_ELAPSED, IDC_RADIO_TIME_REMAINING, IDC_RADIO_TIME_DISABLED } ),
           qwr::ui::CreateUiDdxOption<qwr::ui::UiDdx_CheckBox>( disableWhenPaused_, IDC_CHECK_DISABLE_WHEN_PAUSED ),
